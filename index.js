90edf611c796ed25a14f1e048e3aa5ec622fe32b
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-03-29 10:44:54 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+    //use querySelector to change the fontSize on the h1 element
+
+
+    //use quesrySelector and innerHTML to add text using #firstDiv id
+
+
+    //use querySelectorAll to change all p tags to a different color
+
+
+    //use getElementByClassName to select the paragraph class and set it's display to none
+
+
+    //use getElementsById and innerHTML to add text using #secondDiv id
+    //change the font size to 64px
+    //add an h1 element inside the div
+    //add id to the h1 you just created
+    //add border around h1
