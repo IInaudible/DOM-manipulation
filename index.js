bb53d1d6cbdf79eba5cc7df82f7f9633bef9e0b6
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-03-29 14:39:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
     //use querySelector to change the fontSize on the h1 element
 
 
-    //use quesrySelector and innerHTML to add text using #firstDiv id
+    //use querySelector and innerHTML to add text using #firstDiv id
 
 
     //use querySelectorAll to change all p tags to a different color
 
 
-    //use getElementByClassName to select the paragraph class and set it's display to none
+    //use querySelector to select the paragraph class and set it's display to none
 
 
     //use getElementsById and innerHTML to add text using #secondDiv id
